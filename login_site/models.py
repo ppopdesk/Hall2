9424211a8d1add1db6776e84659b23a1098d4b17
Vijaya Anand <sunrockers8@gmail.com>
2023-02-13 01:39:09 +0530

    Uploaded current hall list & username -> roll number

--- login_site/models.py
+++ login_site/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 #Create Models Here
 
@@ -7,3 +8,6 @@ class User_OTP(models.Model):
     username = models.CharField(null=True,max_length=150)
     email = models.EmailField(null=True,max_length=50)
     otp_generated = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.username + ' ' + str(datetime.now())
--- userprofile/views.py
+++ userprofile/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from hall2temp.settings import EMAIL_HOST_USER
 from .forms import MailForm
+from .models import student
+from django.core.files.storage import FileSystemStorage
+import pandas as pd
 # Create your views here.
 
 #View 1 : Shows Profile Page of the User upon login
@@ -29,3 +32,23 @@ def mail_hec(request):
 
 def mail_sent(request):
     return render(request,'formsent.html')
+
+@login_required
+def import_excel(request):
+    if request.user.is_staff:
+        if request.method == 'POST' and request.FILES['students_data']:      
+            myfile = request.FILES['students_data']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            empexceldata = pd.read_excel(filename)    
+            dbframe = empexceldata
+            for dbframe in dbframe.itertuples():
+                obj = student.objects.create(name=dbframe.name,roll_number=int(dbframe.roll_number))           
+                obj.save()
+            return redirect('../formsent/')
+        return render(request, 'import_excel_db.html')
+    else:
+        return render(request,"404error.html")
+
+def form_sent_view(request):
+    return render(request,"formsent.html")
