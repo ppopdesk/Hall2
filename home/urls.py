cac057c76c802dca42c6682990389989129593a8
Vijaya Anand <sunrockers8@gmail.com>
2023-02-26 21:56:40 +0530

    Changes in homepage

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/',views.home_view,name='home'),
+    path('about/',views.about_view,name='about'),
+    path('about/',views.mess_view,name='mess_about'),
 ]
